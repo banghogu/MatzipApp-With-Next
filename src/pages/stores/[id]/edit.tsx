25c9211ea49024b5b65d40f1e7228eb83569eccf
hojin <devbang5674@gmail.com>
2024-05-02 21:02:11 +0900

    5/2 feat:맛집 수정 삭제기능 완성

--- src/pages/stores/[id]/edit.tsx
+++ src/pages/stores/[id]/edit.tsx
@@ -1,10 +1,36 @@
-import { useRouter } from 'next/router'
-import React from 'react'
+import { useRouter } from "next/router";
+import { useQuery } from "react-query";
+import { getStore } from "@/remote/store";
+import Loader from "@/components/shared/Loader";
+import EditStoreForm from "@/components/EditStore.tsx/EditStoreForm";
 
-const StoreEditPage = () => {
-  const router = useRouter()
-  const { id } = router.query as { id: string }
-  return <div>StoreEditPage {id}</div>
+export default function StoreEditPage() {
+  const router = useRouter();
+  const { id } = router.query as { id: string };
+
+  const {
+    data: store,
+    isFetching,
+    isError,
+  } = useQuery(`store-${id}`, () => getStore(id), {
+    refetchOnWindowFocus: false,
+  });
+
+  if (isError) {
+    return (
+      <div className="w-full h-screen mx-auto pt-[10%] text-red-500 text-center font-semibold">
+        다시 시도해주세요
+      </div>
+    );
+  }
+
+  if (isFetching) {
+    return <Loader className="mt-[20%]" />;
+  }
+
+  if (store == null) {
+    return;
+  }
+
+  return <EditStoreForm data={store} />;
 }
-
-export default StoreEditPage
--- src/pages/stores/[id]/index.tsx
+++ src/pages/stores/[id]/index.tsx
@@ -2,6 +2,9 @@ import Map from "@/components/home/Map";
 import Loader from "@/components/shared/Loader";
 import Marker from "@/components/store/Marker";
 import { getStore } from "@/remote/store";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import { useQuery } from "react-query";
@@ -9,6 +12,7 @@ import { useQuery } from "react-query";
 const StoreDetailPage = () => {
   const router = useRouter();
   const { id } = router.query as { id: string };
+  const { status } = useSession();
 
   const {
     data: store,
@@ -27,20 +31,59 @@ const StoreDetailPage = () => {
     );
   }
 
+  const handleDelete = async () => {
+    const confirm = window.confirm("해당 가게를 삭제하시겠습니까?");
+
+    if (confirm && store) {
+      try {
+        const result = await axios.delete(`/api/stores?id=${store?.id}`);
+
+        if (result.status === 200) {
+          router.replace("/");
+        } else {
+          alert("다시 시도해주세요.");
+        }
+      } catch (e) {
+        console.log(e);
+        alert("다시 시도해주세요.");
+      }
+    }
+  };
+
   if (isFetching) {
     return <Loader className="mt-[20%]" />;
   }
   return (
     <>
       <div className="max-w-5xl mx-auto px-4 py-8 mt-14">
-        <div className="px-4 sm:px-0">
-          <h3 className="text-base font-semibold leading-7 text-gray-900">
-            {store?.name}
-          </h3>
-          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
-            {store?.address}
-          </p>
+        <div className="md:flex justify-between items-center py-4 md:py-0">
+          <div className="px-4 sm:px-0">
+            <h3 className="text-base font-semibold leading-7 text-gray-900">
+              {store?.name}
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+              {store?.address}
+            </p>
+          </div>
+          {status === "authenticated" && (
+            <div className="flex items-center gap-4 px-4 py-3">
+              <Link
+                className="underline hover:text-gray-400 text-sm"
+                href={`/stores/${store?.id}/edit`}
+              >
+                수정
+              </Link>
+              <button
+                type="button"
+                onClick={handleDelete}
+                className="underline hover:text-gray-400 text-sm"
+              >
+                삭제
+              </button>
+            </div>
+          )}
         </div>
+
         <div className="mt-6 border-t border-gray-100">
           <dl className="divide-y divide-gray-100">
             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
