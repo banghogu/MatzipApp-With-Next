b39e2f39f0fc487d1cf1c97dd2fc4c41c5a2c338
hojin <devbang5674@gmail.com>
2024-05-03 21:14:09 +0900

    5/3 fix: getStaticProps오류 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,14 +16,14 @@ export default function Home({ stores }: { stores: StoreType[] }) {
   );
 }
 
-export async function getStaticProps() {
-  const stores = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/stores`
-  );
-  return {
-    props: {
-      stores: stores?.data,
-      revalidate: 60 * 60,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   const stores = await axios.get(
+//     `${process.env.NEXT_PUBLIC_API_URL}/api/stores`
+//   );
+//   return {
+//     props: {
+//       stores: stores?.data,
+//       revalidate: 60 * 60,
+//     },
+//   };
+// }
